7db221ab38cab48bdb969c0347b226cc7df6fc71
sun <sweddie@naver.com>
2019-10-22 20:09:14 +0900

    water level has been added

--- tumbler-main/tumbler-main.ino
+++ tumbler-main/tumbler-main.ino
@@ -1,9 +1,10 @@
 int trigpin=6;
 int echopin=7;
 
-int led1 =11;
+int led1 =9;
 int led2 =10;
-int led3 =9;
+int led3 =11;
+int led4=3;
 
 void setup() {
  Serial.begin(9600);
@@ -14,6 +15,7 @@ void setup() {
  pinMode(led1,OUTPUT);
  pinMode(led2,OUTPUT);
  pinMode(led3,OUTPUT);
+ pinMode(led4,OUTPUT);
 
  delay(1000);
 }
@@ -30,22 +32,32 @@ void loop() {
   distance=(duration / 2) / 29.1;
   Serial.println("cm: ");
   Serial.println(distance);
-
-  if( (distance >0) &&  (distance <= 3) )
+ ///뚜껑에 초음파센서를 부착하며 1번led가 맨아래
+  if( (distance >0) &&  (distance <= 3) )///물이 충분할때
   {digitalWrite(led1,LOW);
-   digitalWrite(led2,HIGH);
-   digitalWrite(led3,HIGH);
+   digitalWrite(led2,LOW);
+   digitalWrite(led3,LOW);
+   digitalWrite(led4,LOW);
   } else
-  if( (distance > 3) && (distance <= 6)) 
+  if( (distance > 3) && (distance <= 6))///물 중간
   {
   digitalWrite(led1,HIGH);
   digitalWrite(led2,LOW);
-  digitalWrite(led3,HIGH);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,LOW);
   } else
-  if( (distance > 6) && (distance <= 12) )
+  if( (distance > 6) && (distance <= 9 ) )///물 중간
   {
     digitalWrite(led1,HIGH);
     digitalWrite(led2,HIGH);
     digitalWrite(led3,LOW);
+    digitalWrite(led4,LOW);
+  }
+  if( (distance > 9) && (distance <= 15 ) )///물 부족
+  {
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    digitalWrite(led3,HIGH);
+    digitalWrite(led4,LOW);
   }
 }
